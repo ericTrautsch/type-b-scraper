ccef71df079a707a5b16f82ec5f70546d77e06c6
Eric Trautsch <erictrautsch@outlook.com>
2025-02-13 14:25:17 -0600

    List of links of all the pdfs collected.

--- src/extract.py
+++ src/extract.py
@@ -1,11 +1,28 @@
+from bs4 import BeautifulSoup
 import requests
 
 
-def scrape_data(url):
-    return False
+def extract_links(url):
+    """
+    Extracts links for relative download urls for part b fee schedules.
+    """
+    hostname = "/".join(url.split("/")[0:3])
+    # TODO: Determine headers (if needed)
+    headers = {"User-Agent": "Mozilla/5.0"}
+
+    # Get top-level page and create parser
+    response = requests.get(url, headers)
+    soup = BeautifulSoup(response.text, "html.parser")
+
+    # Find all cmp-buttons
+    buttons = soup.find_all("a", class_="cmp-button")
+
+    # Extract links
+    links = [hostname + button["href"] for button in buttons if "href" in button.attrs]
+    return links
 
 
 if __name__ == "__main__":
     scrape_url = "https://pa.gov/agencies/dli/programs-services/workers-compensation/wc-health-care-services-review/wc-fee-schedule/part-b-fee-schedules.html"
-    print(scrape_data(scrape_url))
-    print(requests.get(scrape_url).content)
+    links = extract_links(scrape_url)
+    print(links)
